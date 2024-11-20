3c22e61e7dcf8963215bf4b4e1f58dbfa52f4bb2
Pascal Molli <pascal.molli@ls2n.fr>
2024-11-20 10:45:42 +0100

    refactor

--- SPARQLLM/udf/segraph.py
+++ SPARQLLM/udf/segraph.py
@@ -13,50 +13,45 @@ import json
 import hashlib
 
 from SPARQLLM.udf.SPARQLLM import store
+from SPARQLLM.config import ConfigSingleton
+from SPARQLLM.utils.utils import print_result_as_table, named_graph_exists
 
 import logging
+logger = logging.getLogger(__name__)
 
 # https://console.cloud.google.com/apis/api/customsearch.googleapis.com/cost?hl=fr&project=sobike44
 se_api_key=os.environ.get("SEARCH_API_SOBIKE44")
 se_cx_key=os.environ.get("SEARCH_CX")
-
 
 headers = {
     'Accept': 'text/html',
     'User-Agent': 'Mozilla/5.0'  # En-tête optionnel pour émuler un navigateur
 }
 
-def named_graph_exists(conjunctive_graph, graph_uri):
-    for g in conjunctive_graph.contexts():  # context() retourne tous les named graphs
-        if g.identifier == graph_uri:
-            return True
-    return False
-
 # link_to should be UrI.
 def SEGRAPH(keywords,link_to):
     global store
-    logging.debug(f"SEGRAPH: id Store {id(store)}")
 
-    #print(f"Google store: {id(store)}")
-    logging.debug(f"SEGRAPH: ({keywords},{link_to},{type(link_to)})")
+    config = ConfigSingleton()
+    se_url = config.config['Requests']['SLM-CUSTOM-SEARCH-URL'].format(se_cx_key=se_cx_key,se_api_key=se_api_key)
+    max_links = int(config.config['Requests']['SLM-SEARCH-MAX-LINKS'])
+
+    logger.debug(f"SEGRAPH: ({keywords},{link_to},{type(link_to)},se_url:{se_url}, max_links:{max_links})")
 
     if not isinstance(link_to,URIRef) :
         raise ValueError("SEGRAPH 2nd Argument should be an URI")
 
-    se_url=f"https://customsearch.googleapis.com/customsearch/v1?cx={se_cx_key}&key={se_api_key}"
-
     graph_uri = URIRef("http://google.com/"+hashlib.sha256(keywords.encode()).hexdigest())
     if  named_graph_exists(store, graph_uri):
-        logging.debug(f"Graph {graph_uri} already exists (good)")
+        logger.debug(f"Graph {graph_uri} already exists (good)")
         return graph_uri
     else:
-
         named_graph = store.get_context(graph_uri)
 
         # Send the request to Google search
         se_url = f"{se_url}&q={quote(keywords)}"
 
-        logging.debug(f"se_url={se_url}")
+        logger.debug(f"se_url={se_url}")
 
         headers = {'Accept': 'application/json'}
         request = Request(se_url, headers=headers)
@@ -64,48 +59,29 @@ def SEGRAPH(keywords,link_to):
         json_data = json.loads(response.read().decode('utf-8'))
 
         links = [item['link'] for item in json_data.get('items', [])]
-        logging.debug(f"SEGRAPH got nb links:{len(links)}")        
+        logger.debug(f"SEGRAPH got nb links:{len(links)}")        
 
-        # Extract the URLs from the response
 #        for item in json_data.get('items', []) :
-        for item in links[:1]:
+        for item in links[:max_links]:
             #print(f"Adding {item['link']} to {link_to}")
-            logging.debug(f"SEGRAPH found: {item}")
+            logger.debug(f"SEGRAPH found: {item}")
             named_graph.add((link_to, URIRef("http://example.org/has_uri"), URIRef(item)))        
             #for s, p, o in named_graph:
             #    print(f"Subject: {s}, Predicate: {p}, Object: {o}")
         return graph_uri
 
 
-# Register the function with a custom URI
-register_custom_function(URIRef("http://example.org/SEGRAPH"), SEGRAPH)
 
-
-
+# run with : python -m SPARQLLM.udf.segraph
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    config = ConfigSingleton(config_file='config.ini')
 
+    # Register the function with a custom URI
+    register_custom_function(URIRef("http://example.org/SEGRAPH"), SEGRAPH)
 
     # Add some sample data to the graph
     store.add((URIRef("http://example.org/subject1"), URIRef("http://example.org/hasValue"), Literal("university nantes", datatype=XSD.string)))  
-
-    # # SPARQL query using the custom function
-    # query_str = """
-    #     PREFIX ex: <http://example.org/>
-    #     SELECT ?s ?uri
-    #     WHERE {
-    #         ?s ?p ?value .
-    #         BIND(ex:SEGRAPH(REPLACE("trouve moi une url pour UNIV ","UNIV",STR(?value)),?s) AS ?graph)
-    #         GRAPH ?graph {?s <http://example.org/has_uri> ?uri}    
-    #     }
-    #     """
-
-    # # Execute the query
-    # query = prepareQuery(query_str)
-    # result = store.query(query)
-
-    # # Display the results
-    # for row in result:
-    #     print(f"Result : {row}")
 
     query_str = """
         PREFIX ex: <http://example.org/>
@@ -121,11 +97,7 @@ if __name__ == "__main__":
         """
 
     # Execute the query
-    query = prepareQuery(query_str)
-    result = store.query(query)
-
-    # Display the results
-    for row in result:
-        print(f"Result : {row}")
+    result = store.query(query_str)
+    print_result_as_table(result)
 
 
