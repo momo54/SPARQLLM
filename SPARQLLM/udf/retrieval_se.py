26f844e5c460023c2c5400f5c246414ac6e0bcc9
Denez <denez.guerx@gmail.com>
2025-01-29 14:16:06 +0100

    int fixed with sparql

--- SPARQLLM/udf/retrieval_se.py
+++ SPARQLLM/udf/retrieval_se.py
@@ -1,5 +1,5 @@
 from re import search
-
+import os
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 import logging
@@ -9,23 +9,27 @@ embeddings = OllamaEmbeddings(
     model="jina/jina-embeddings-v2-small-en"
 )
 db_name = "pset_vector_store"
-def retrieval_se(query, k=5):
-
+def retrieval_se(query, nb_result=5):
+    files = os.listdir("pset_vector_store")
+    to_string_files = str(files)
     logger.debug(f"Query: {query}")
+    n = int(nb_result)
     ##Load the local vector store if existing
     vector_store = FAISS.load_local(db_name, embeddings=embeddings, allow_dangerous_deserialization=True)
-    #chunks = vector_store.search(query,search_type="similarity")
+    chunks = vector_store.similarity_search(query=query,k=n)
     retriever = vector_store.as_retriever(search_type="mmr",
-                                          search_kwargs={'k': k,
+                                          search_kwargs={'k': n,
                                                         'fetch_k':100,
                                                         'lambda_mult': 1})
-    chunks = retriever.invoke(query)
+    #chunks = retriever.invoke(query)
+    logger.debug(f"chunks ready")
     result = []
     for chunk in chunks:
         result.append(chunk.page_content)
-
-    logger.debug(f"Result: {len(result)}")
-    print(result)
+        print(chunk)
+        print("\n\n")
+        print("=====================================================================")
+        print("\n\n")
     return result
 
 if __name__ == "__main__":
