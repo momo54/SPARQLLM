032edd8a73909a245fe47784730a3b7469dd27bd
Pascal Molli <pascal.molli@ls2n.fr>
2025-03-06 14:08:02 +0100

    no need to include openai for testing simple UDFs

---
+++ config.fs
@@ -0,0 +1,12 @@
+[Associations]
+SLM-FILE = SPARQLLM.udf.absPath.absPath
+SLM-CSV = SPARQLLM.udf.csv.slm_csv
+SLM-GETTEXT = SPARQLLM.udf.uri2text.GETTEXT
+SLM-SCHEMA = SPARQLLM.udf.schemaorg.SCHEMAORG
+SLM-READDIR = SPARQLLM.udf.readdir.RDIR
+SLM-READHTMLFILE = SPARQLLM.udf.readfile.readhtmlfile
+SLM-RECURSE = SPARQLLM.udf.recurse.recurse
+
+[Requests]
+; if we need to truncate some long text
+SLM-TRUNCATE=5000
